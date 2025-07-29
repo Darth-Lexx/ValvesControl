31483f2195decced074334676e0ba895fee1a741
Darth-Lexx <116996438+Darth-Lexx@users.noreply.github.com>
2025-07-29 16:18:59 +0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,85 @@
+#include "main.h"
+
+void setup()
+{
+  Serial.begin(115200);
+  // установка пинов ресета, кнопок и реле
+  pinMode(PIN_RESET, INPUT_PULLUP);
+  pinMode(PIN_BTN_CH1, INPUT_PULLUP);
+  pinMode(PIN_BTN_CH2, INPUT_PULLUP);
+  pinMode(PIN_BTN_CH3, INPUT_PULLUP);
+  pinMode(PIN_BTN_CH4, INPUT_PULLUP);
+  pinMode(PIN_BTN_H, INPUT_PULLUP);
+  pinMode(PIN_BTN_L, INPUT_PULLUP);
+  pinMode(PIN_BTN_N2, INPUT_PULLUP);
+
+  pinMode(PIN_H_VALVE, OUTPUT);
+  pinMode(PIN_L_VALVE, OUTPUT);
+  pinMode(PIN_N2_VALVE, OUTPUT);
+  pinMode(PIN_H_REL, OUTPUT);
+  pinMode(PIN_L_REL, OUTPUT);
+  pinMode(PIN_N2_REL, OUTPUT);
+  pinMode(PIN_CTRL1_POWER, OUTPUT);
+  pinMode(PIN_CTRL2_POWER, OUTPUT);
+  pinMode(PIN_CTRL3_POWER, OUTPUT);
+  pinMode(PIN_CTRL4_POWER, OUTPUT);
+  digitalWrite(PIN_H_VALVE, PIN_OFF);
+  digitalWrite(PIN_L_VALVE, PIN_OFF);
+  digitalWrite(PIN_N2_VALVE, PIN_OFF);
+  digitalWrite(PIN_CTRL1_POWER, PIN_OFF);
+  digitalWrite(PIN_CTRL2_POWER, PIN_OFF);
+  digitalWrite(PIN_CTRL3_POWER, PIN_OFF);
+  digitalWrite(PIN_CTRL4_POWER, PIN_OFF);
+
+  // открытие клапанов сброса давления
+  digitalWrite(PIN_H_REL, PIN_ON);
+  digitalWrite(PIN_L_REL, PIN_ON);
+  digitalWrite(PIN_N2_REL, PIN_ON);
+
+  delay(2000);
+
+  // закрытие клапанов сброса давления
+  digitalWrite(PIN_H_REL, PIN_OFF);
+  digitalWrite(PIN_L_REL, PIN_OFF);
+  digitalWrite(PIN_N2_REL, PIN_OFF);
+
+  // включение AS200
+  digitalWrite(PIN_CTRL1_POWER, PIN_ON);
+  digitalWrite(PIN_CTRL2_POWER, PIN_ON);
+  digitalWrite(PIN_CTRL3_POWER, PIN_ON);
+  digitalWrite(PIN_CTRL4_POWER, PIN_ON);
+
+  // проверка пина сброса настроек
+  bool reset = !digitalRead(PIN_RESET);
+  // чтение данных из EEPROM
+  SlaveMbAdr = EEPROM.read(EEPROM_ADR);
+  SlaveMbSpeed = EEPROM.read(EEPROM_SPEED);
+  Delta = EEPROM.read(EEPROM_DELTA);
+  EEPROM.get(EEPROM_FLOW, Flow);
+
+  // проверка валидности прочитанных данных (сброс если не валидные или установлен пин сброса)
+  if (SlaveMbAdr == 0 || SlaveMbAdr > 247 || reset)
+  {
+    SlaveMbAdr = MBSL_ADR_D;
+    EEPROM.update(EEPROM_ADR, SlaveMbAdr);
+  }
+  if ((SlaveMbSpeed != 1 && SlaveMbSpeed != 2 && SlaveMbSpeed != 4 && SlaveMbSpeed != 8 && SlaveMbSpeed != 12 && SlaveMbSpeed != 24) || reset)
+  {
+    SlaveMbSpeed = 2;
+    EEPROM.update(EEPROM_SPEED, SlaveMbSpeed);
+  }
+  if (Delta < 30 || Delta > 100 || reset)
+  {
+    Delta = MBSL_DELTA_D;
+    EEPROM.update(EEPROM_DELTA, Delta);
+  }
+  if (Flow < 400 || Flow > 1000 || reset)
+  {
+    Flow = MBSL_FLOW_D;
+    EEPROM.put(EEPROM_FLOW, Flow);
+  }
+}
+
+void loop()
+{
+}
---
+++ src/main.h
@@ -0,0 +1,110 @@
+#ifndef ValvesControl_H
+#define ValvesControl_H
+
+#include <Arduino.h>
+
+#define EB_NO_FOR           // отключить поддержку pressFor/holdFor/stepFor и счётчик степов (экономит 2 байта оперативки)
+#define EB_NO_CALLBACK      // отключить обработчик событий attach (экономит 2 байта оперативки)
+#define EB_NO_COUNTER       // отключить счётчик энкодера (экономит 4 байта оперативки)
+#define EB_NO_BUFFER        // отключить буферизацию энкодера (экономит 1 байт оперативки)
+#define EB_DEB_TIME     50      // таймаут гашения дребезга кнопки (кнопка)
+#define EB_CLICK_TIME   200   // таймаут ожидания кликов (кнопка)
+#define EB_HOLD_TIME    300    // таймаут удержания (кнопка)
+#define EB_STEP_TIME    500
+#include <EncButton.h>
+
+#include <ModbusRTUSlave.h>
+#include <ModbusRTUMaster.h>
+
+#include <EEPROM.h>
+
+#define array_count(a) sizeof(a) / sizeof((a)[0])
+
+bool Channel1 = false, Channel2 = false, Channel3 = false, Channel4 = false;
+
+//--ModBus slave registers  --//
+#define MBSL_R_ADR          0       //rw
+#define MBSL_R_SPEED        1
+#define MBSL_R_FLOW         2
+#define MBSL_R_DELTA        3
+#define MBSL_R_CH1_VH       4       //ro
+#define MBSL_R_CH1_VL       5
+#define MBSL_R_CH2_VH       6
+#define MBSL_R_CH2_VL       7
+#define MBSL_R_CH3_VH       8
+#define MBSL_R_CH3_VL       9
+#define MBSL_R_CH4_VH       10
+#define MBSL_R_CH4_VL       11
+#define MBSL_ERROR          12      //старший байт - флаги каналов, младший байт - 0 и 1 биты - флаги ошибки первого канала
+//--//
+
+//--default values
+#define MBSL_ADR_D          3
+#define MBSL_SPEED_D        2
+#define MBSL_DELTA_D        30
+#define MBSL_FLOW_D         600
+
+//--ModBus slave coils      --//
+#define MBSL_C_H           0       //rw
+#define MBSL_C_L           1
+#define MBSL_C_N2           2
+#define MBSL_C_CH1          3
+#define MBSL_C_CH2          4
+#define MBSL_C_CH3          5
+#define MBSL_C_CH4          6
+#define MBSL_C_CH1_READY    7       //ro
+#define MBSL_C_CH2_READY    8
+#define MBSL_C_CH3_READY    9
+#define MBSL_C_CH4_READY    10
+//--//
+
+#define SlaveSerial Serial1
+#define MasterSerial Serial2
+
+//--Pins                    --//
+#define PIN_ON              1
+#define PIN_OFF             0
+
+#define PIN_RESET           2
+#define PIN_H_VALVE        3
+#define PIN_L_VALVE        4
+#define PIN_N2_VALVE        5
+#define PIN_H_REL          6
+#define PIN_L_REL          7
+#define PIN_N2_REL          8
+#define PIN_CTRL1_POWER     9
+#define PIN_CTRL2_POWER     10
+#define PIN_CTRL3_POWER     11
+#define PIN_CTRL4_POWER     12
+
+#define PIN_BTN_CH1         22
+#define PIN_BTN_CH2         23
+#define PIN_BTN_CH3         24
+#define PIN_BTN_CH4         25
+
+#define PIN_BTN_H          26
+#define PIN_BTN_L          27
+#define PIN_BTN_N2          28
+#define PIN_DE              53
+
+//--EEPROM                  --//
+#define EEPROM_ADR          0
+#define EEPROM_SPEED        1
+#define EEPROM_FLOW         2 //float - 4b
+#define EEPROM_DELTA        6
+
+ButtonT<PIN_BTN_CH1> Channel1Button;
+ButtonT<PIN_BTN_CH2> Channel2Button;
+ButtonT<PIN_BTN_CH3> Channel3Button;
+ButtonT<PIN_BTN_CH4> Channel4Button;
+ButtonT<PIN_BTN_H> HighButton;
+ButtonT<PIN_BTN_L> LowButton;
+ButtonT<PIN_BTN_N2> N2Button;
+
+ModbusRTUSlave MbSlave(SlaveSerial);
+ModbusRTUMaster MbMaster(MasterSerial, PIN_DE);
+
+byte SlaveMbAdr, SlaveMbSpeed, Delta;
+float Flow;
+
+#endif
